d793aac73e2b0da8c9c8497bc66b901c11bda3e1
sun <837544713@qq.com>
2019-02-22 11:27:48 +0800

    init

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+import os.path
+import codecs
+
+from numpy import unicode
+
+filePaths=[]
+fileContents=[]
+''''
+for root,dirs,files in os.walk('/home/sun/Documents'):
+    for name in files:
+        filePath=os.path.join(root,name)
+        filePaths.append(filePath)
+        f=codecs.open(filePath,'r','utf-8')
+        fileContent=f.read()
+        f.close()
+        fileContents.append(fileContent)
+
+import pandas
+corpos=pandas.DataFrame({
+    'filePath':filePaths,
+    'fileContent':fileContents
+})
+print(corpos)
+'''
+import jieba
+import jieba.analyse
+words=jieba.cut("漂亮可爱被骚浩嫌弃的杨苗苗是个王八蛋")
+print("/".join(words))
+tags = jieba.analyse.extract_tags(words, topK=100, withWeight=False)
+text=" ".join(tags)
+#text=unicode(text)
+print(text)
